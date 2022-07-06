308d3dd6908d6748e9e111180afbb0948cefbf5a
Sharry Xu <sharry.xu@outlook.com>
2022-07-06 14:25:05 +0800

    Keep learning.

--- lec5/test_radix_sort.py
+++ lec5/test_radix_sort.py
@@ -5,7 +5,7 @@ from radix_sort import radix_sort
 
 class TestRadixSort:
     def test(self):
-        A = [randint(10, 100) for i in range(10)]
+        A = [randint(10, 1000) for i in range(30)]
         A = radix_sort(A)
         for i in range(len(A) - 1):
             assert A[i] <= A[i + 1]
